7f7ad9f8156015a11beba04d277b7e19d372d84c
iva7777 <iva.kesten@gmail.com>
2022-01-02 23:42:20 +0200

    Added Models

---
+++ LearningSystem/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningSystem.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            this.EnrolledCourses = new List<StudentsInCourses>();
+            this.LecturerCourses = new List<CourseInstance>();
+            this.HomeworkSubmitions = new List<HomeworkSubmition>();
+            this.Articles = new List<Article>();
+        }
+
+        public string FullName { get; set; }
+
+        public ICollection<StudentsInCourses> EnrolledCourses { get; set; }
+
+        public ICollection<CourseInstance> LecturerCourses { get; set; }
+
+        public ICollection<HomeworkSubmition> HomeworkSubmitions { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ LearningSystem/Models/QuestionPage.cs
@@ -0,0 +1,13 @@
+﻿namespace LearningSystem.Models
+{
+    using System.Collections.Generic;
+
+    public class QuestionPage
+    {
+        public object Id { get; set; }
+
+        public string QuestionSlug { get; set; }
+
+        public ICollection<Question> Questions { get; set; }
+    }
+}
